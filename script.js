14b71af4f2af34545340cf230344c48bf71aa291
Burhan Mullamitha <burhanmm1998@gmail.com>
2020-06-20 01:56:26 +0100

    Finished media queries for homepage

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
+// Navigation Menu
+let menu = document.querySelector('.navbar__mobile');
+let overlay = document.querySelector('.overlay');
+
+menu.addEventListener('click', function() {
+    this.classList.toggle('active');
+    this.parentElement.children[2].classList.toggle('active');
+    overlay.classList.toggle('active');
+
+    this.classList.contains('active') ? document.body.style.overflow = 'hidden' : document.body.style.overflow = 'visible';
+})
+
 // FAQ Accordion
 
 let items = document.querySelectorAll('.accordion__item');
 
 items.forEach(item => {
-    // console.log(item);
     item.addEventListener('click', function() {
         let answer = this.children[1];
         let currentArrow = this.children[0].children[1];
